da9b581959cdfabc6a881d355bf34add6ea17925
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-12-01 13:41:30 +0300

    Update stream_manager.py

--- stream_manager.py
+++ stream_manager.py
@@ -13,7 +13,7 @@ class Stream:
         self.input_url = input_url
         self.rtmp = rtmp
         self.title = title
-        self.input_type = input_type
+        self.input_type = input_type  # m3u8, yt, iptv, embed
         self.start_time = datetime.datetime.utcnow()
         self.process = None
         self.thread = None
@@ -24,35 +24,33 @@ class Stream:
     def set_chat_id(self, chat_id):
         self.chat_id = chat_id
 
-    def _run_gstreamer(self):
+    def _run_ffmpeg(self):
         while self.running:
             cmd = [
-                "gst-launch-1.0", "-e",
-                "souphttpsrc", f"location={self.input_url}", "is-live=true",
-                "!", "hlsdemux",
-                "!", "decodebin", "name=dec",
-                "dec.", "!", "queue", "!", "videoconvert", "!", 
-                "x264enc", "bitrate=4500", "speed-preset=veryfast", "tune=zerolatency", "key-int=30",
-                "!", "video/x-h264,stream-format=byte-stream",
-                "!", "flvmux", "streamable=true", "name=mux",
-                "dec.", "!", "queue", "!", "audioconvert", "!", "audioresample", "!", 
-                "aacenc", "bitrate=128000",
-                "!", "mux.",
-                "mux.", "!", "rtmpsink", f"location={self.rtmp}"
+                "ffmpeg", "-y",
+                "-fflags", "+genpts+nobuffer", "-avoid_negative_ts", "make_zero",
+                "-reconnect", "1", "-reconnect_at_eof", "1",
+                "-reconnect_streamed", "1", "-reconnect_delay_max", "10",
+                "-timeout", "30000000"
             ]
 
             if self.input_type == "yt":
-                # For YouTube, use playbin with loop
-                cmd = [
-                    "gst-launch-1.0", "-e",
-                    "playbin", f"uri={self.input_url}", "flags=0x10",
-                    "video-sink=videoconvert ! x264enc bitrate=4500 speed-preset=veryfast tune=zerolatency key-int=30 ! flvmux streamable=true ! rtmpsink location=" + self.rtmp,
-                    "audio-sink=audioconvert ! audioresample ! aacenc bitrate=128000 ! flvmux streamable=true ! rtmpsink location=" + self.rtmp
-                ]
+                cmd += ["-stream_loop", "-1"]
+
+            cmd += [
+                "-re", "-i", self.input_url,
+                "-c:v", "libx264", "-preset", "veryfast", "-tune", "zerolatency",
+                "-b:v", "4500k", "-maxrate", "5000k", "-bufsize", "15000k",
+                "-g", "30", "-keyint_min", "30", "-r", "30", "-pix_fmt", "yuv420p",
+                "-c:a", "aac", "-b:a", "128k", "-ar", "44100",
+                "-af", "aresample=async=1:first_pts=0",
+                "-f", "flv", "-flvflags", "+add_keyframe_index",
+                "-rtmp_buffer", "1000", "-rtmp_live", "live",
+                self.rtmp
+            ]
 
             proc = subprocess.Popen(
-                " ".join(cmd),
-                shell=True,
+                cmd,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.PIPE
             )
@@ -71,7 +69,7 @@ class Stream:
                             text=f"*Stream Stopped*\n\n"
                                  f"Title: `{self.title}`\n"
                                  f"ID: `{self.id}`\n"
-                                 f"Reason: Connection lost",
+                                 f"Reason: Source lost",
                             parse_mode="Markdown"
                         )
                     )
@@ -82,22 +80,19 @@ class Stream:
             time.sleep(3)
 
     def start(self):
-        if self.running:
-            return
+        if self.running: return
         self.running = True
-        self.thread = threading.Thread(target=self._run_gstreamer, daemon=True)
+        self.thread = threading.Thread(target=self._run_ffmpeg, daemon=True)
         self.thread.start()
 
     def stop(self):
         self.running = False
         if self.process:
             self.process.terminate()
-            try:
-                self.process.wait(5)
-            except:
-                self.process.kill()
+            try: self.process.wait(5)
+            except: self.process.kill()
 
-    def uptime(self) -> str:
+    def uptime(self):
         delta = datetime.datetime.utcnow() - self.start_time
         h, rem = divmod(int(delta.total_seconds()), 3600)
         m, s = divmod(rem, 60)
@@ -110,21 +105,11 @@ class Stream:
 class StreamManager:
     def __init__(self):
         self.streams = {}
-
-    def new_id(self):
-        return str(uuid.uuid4())[:8]
-
-    def add(self, stream: Stream):
-        self.streams[stream.id] = stream
-
-    def get(self, stream_id: str) -> Stream | None:
-        return self.streams.get(stream_id)
-
-    def remove(self, stream_id: str):
-        self.streams.pop(stream_id, None)
-
+    def new_id(self): return str(uuid.uuid4())[:8]
+    def add(self, s): self.streams[s.id] = s
+    def get(self, sid): return self.streams.get(sid)
+    def remove(self, sid): self.streams.pop(sid, None)
     def all(self):
         dead = [sid for sid, s in self.streams.items() if not s.is_running()]
-        for sid in dead:
-            del self.streams[sid]
+        for sid in dead: del self.streams[sid]
         return list(self.streams.values())
